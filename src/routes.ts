681e477a097dccdc4a227846a3df9ed20f00f4b0
Ariele Mendonça <ariele_apm@outlook.com>
2021-06-30 22:56:14 -0300

    Add authenticate with jwt

--- src/routes.ts
+++ src/routes.ts
@@ -2,14 +2,18 @@ import { Router } from "express";
 import { ensureAdmin } from "./middlewares/ensureAdmin";
 import { CreateUserController } from "./controllers/CreateUserController";
 import { CreateTagController } from "./controllers/CreateTagController";
+import { AuthenticateUserController } from "./controllers/AuthenticateUserController";
 
 const router = Router();
 
 const createUserController = new CreateUserController();
 const createTagController = new CreateTagController();
+const authenticateUserController = new AuthenticateUserController();
 
 router.post("/users", createUserController.handle);
 
 router.post("/tags", ensureAdmin, createTagController.handle);
+
+router.post("/login", authenticateUserController.handle);
 
 export { router };
